89c4c8eddcbf18e6b63c2616d5f21f7efd3c1bfb
Salman Bahaev <bahaev.sm@gmail.com>
2023-04-19 22:41:10 +0300

    serv

--- js/main.js
+++ js/main.js
@@ -3,5 +3,17 @@ $(document).ready(function(){
     variableWidth: true,
   });
 
+  $('.feedbackSlider').slick({
+    variableWidth: true,
+  });
+
   $("#phone").mask("+7 (999) 999-99-99");
+
+  $(".slide-toggle").click(function(){ 
+    $(".in__showed").slideToggle(); 
+
+    $(".in__button-text").toggleClass('in__button-text-toggle');
+
+    $(".in__button-img").toggleClass('in__button-img-toggle');
+  });
 })
